3680266f5bacfa87e0931d28fddd60ea0c999d02
Paolo Carrara <paolocarraratux@gmail.com>
2015-04-29 17:43:02 -0300

    database modifications to ensure compabilities with the local database

--- app/database.php
+++ app/database.php
@@ -1,14 +1,14 @@
 <?php
-	$hostname = 'ec2-54-163-238-169.compute-1.amazonaws.com';
+	$dbDriver = getenv ('DBDRIVER');
+	$hostname = getenv ('HOSTNAME');
 	$username = getenv ('PG_USER');
 	$password = getenv ('PG_PASSWORD');
 	$databaseName = 'de73f59ql11316';
 	$port = '5432';
-
 	$connection = null;
 
 	try {
-		$connection = new PDO ("pgsql:host=ec2-54-163-238-169.compute-1.amazonaws.com;dbname=de73f59ql11316", $username, $password);
+		$connection = new PDO ("$dbDriver:host=$hostname;dbname=$databaseName", $username, $password);
 		$connection->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch (PDOException $e) {
